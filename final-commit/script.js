ef6d51f78098ac009d08bf647147523c9c7c7d10
Revan <84303812+TheRevanite@users.noreply.github.com>
2024-01-05 14:54:07 +0530

    Delete final-commit directory

--- final-commit/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('subscribeForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const confirmationMessage = document.getElementById('confirmationMessage');
-    confirmationMessage.innerText = `Thanks for subscribing! We'll send newsletters to ${email}.`;
-    document.getElementById('subscribeForm').reset();
-});
